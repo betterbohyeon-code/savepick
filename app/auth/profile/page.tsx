b50179ec028938890ed583b6ecf61b2954beafff
betterbohyeon-code <better.bohyeon@gmail.com>
2026-07-22 22:50:14 +0900

    Update page.tsx

--- app/auth/profile/page.tsx
+++ app/auth/profile/page.tsx
@@ -83,7 +83,7 @@ function ProfilePageInner() {
 
   return (
     <MobileShell>
-      <div className="min-h-screen flex flex-col">
+      <div className="min-h-screen md:h-full md:min-h-0 flex flex-col">
         <CustomerHeader storeName={storeName} onBack={() => router.back()} />
 
         <div className="px-6 pt-4">
